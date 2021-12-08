596e8eb00000220459ae2521bfb2f18c5ecc4538
DavidALevy <david.levy.levy@btinternet.com>
2021-12-08 14:46:42 +0000

    changes colour and title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useState } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const ToDoList = () => {    
+    const [items, setitems] = useState([])
+    const [input, setInput] = useState("")
+
+    const addHandler = () => {
+      if(input==="")return;
+        let storeditems = [...items]
+        storeditems.push(input)
+        setitems(storeditems) 
+        setInput("");
+    }
+    const handleKeyDown = (event) => {
+      if(input==="")return;
+      else if (event.key === 'Enter') {
+            let storeditems = [...items]
+            storeditems.push(input)
+            setitems(storeditems)
+            setInput("");
+        }}
+
+    const removeHandler = (index) => {
+        let storeditems = [...items]
+        storeditems.splice(index,1)
+        setitems(storeditems)
+    }
+    const clear=()=>{
+        setitems([])              
+    };
+    
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <header className="inputAndClear">                      
+                <input type="text" id="input" value={input} placeholder="press <add to list> or press <enter> to submit"
+                onChange={(event) => setInput(event.target.value)}                
+                onKeyDown={handleKeyDown}         
+                />
+                <button className="addButton"onClick={ addHandler}>add to list</button>
+                <div><button onClick={clear} className="removeAllButton">delete all</button></div>
+            </header>
+
+            {items.map((item, index) => { 
+                return (
+                <div key={index} class="listAndButton">                  
+                <div class="list1">{item}</div>                
+                 <button className="removeButton"onClick={() => removeHandler(index,1)}>delete</button>
+                </div>
+                  )
+                })}     
+
+                
+        </div>        
+    )     
+    }
+export default ToDoList
